22a90022a949890228a6d36f8b65c9943405034e
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2022-06-13 23:56:17 -0500

    Adding string operations sample.

---
+++ python_one_liners/ch1/string_operations.py
@@ -0,0 +1,21 @@
+
+y = " This is lazy\t\n "
+print(y.strip())
+# Remove Whitespace: 'This is lazy'
+print("DrDre".lower())
+# Lowercase: 'drdre'
+print("attention".upper())
+# Uppercase: 'ATTENTION'
+print("smartphone".startswith("smart"))
+# Matches the string's prefix against the argument: True
+print("smartphone".endswith("phone"))
+# Matches the string's suffix against the argument: True
+print("another".find("other"))
+# Match index: 2
+print("cheat".replace("ch", "m"))
+# Replaces all occurrences of the first by the second argument: meat
+print(','.join(["F", "B", "I"]))
+# Glues together all elements in the list using the separator string: F,B,I
+print(len("Rumpelstiltskin"))
+# String length: 15
+print("ear" in "earth")
