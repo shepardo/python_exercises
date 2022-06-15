9077eb0b03d1d2a3e79c5bc7ae9abd9697f0fdc3
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2022-06-15 01:01:01 -0700

    Adding elements sample.

---
+++ python_one_liners/ch1/adding_elements.py
@@ -0,0 +1,13 @@
+# 1. Append
+l = [1, 2, 2]
+l.append(4)
+print(l)
+# [1, 2, 2, 4]
+# 2. Insert
+l = [1, 2, 4]
+l.insert(2, 3)
+print(l)
+# [1, 2, 3, 4]
+# 3. List Concatenation
+print([1, 2, 2] + [4])
+# [1, 2, 2, 4]
